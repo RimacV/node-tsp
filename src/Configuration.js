023328e5103672664967681845b78cc79443d620
RimacV <rimacvaldez@gmx.de>
2018-04-30 16:14:11 +0200

    removed old keys

--- src/Configuration.js
+++ src/Configuration.js
@@ -4,8 +4,8 @@ const configuration = {
         public_port: 10000,
         public_url: '127.0.0.1',
         enable_gzip: process.env.ENABLE_GZIP === 'true',
-        google_backend_api_key: 'AIzaSyCmXajz3vNgq7IePWTChGHWHiM3iz9VS_I',
-        google_frontend_api_key: 'AIzaSyALJENiOZgcfe8GjRrheIysr5WxiExW-s0'
+        google_backend_api_key: '',
+        google_frontend_api_key: ''
 }
 
 module.exports = configuration
